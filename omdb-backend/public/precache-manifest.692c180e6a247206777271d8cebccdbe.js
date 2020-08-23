3894ec99c3f817241672adce8f056793a35308b8
Nitika30 <nitika30051999@gmail.com>
2020-08-23 15:56:08 +0530

    firstcommit

---
+++ omdb-backend/public/precache-manifest.692c180e6a247206777271d8cebccdbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e62a19bf32f2ef2d356f8df5759186b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed9c6233f7a902e07f0e",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "01a2f0a828e94c925db7",
+    "url": "/static/css/main.d180690c.chunk.css"
+  },
+  {
+    "revision": "ed9c6233f7a902e07f0e",
+    "url": "/static/js/2.e57da0af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e57da0af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a2f0a828e94c925db7",
+    "url": "/static/js/main.71740b0d.chunk.js"
+  },
+  {
+    "revision": "9091f47bb88943cb9e61",
+    "url": "/static/js/runtime-main.89d020cb.js"
+  }
+]);
